049624f02c6754b1702044b48ae5f4026a207ca2
Eralia <eralia.katsakiori@outlook.com>
2022-03-04 14:34:33 +0000

    Add PageHeader component

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,10 @@
+function PageHeader(): JSX.Element {
+    return(
+        <header>
+            <h1>My favourite places</h1>
+            <p><small><em>snippets of the loveliest places in my world.</em></small></p>
+        </header>
+    )
+}
+
+export default PageHeader;
