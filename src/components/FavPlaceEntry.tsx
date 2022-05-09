59d03a78035e047fd8d02c756f6a18ce322b2b69
Eralia <eralia.katsakiori@outlook.com>
2022-05-09 20:57:11 +0100

    update formatting

--- src/components/FavPlaceEntry.tsx
+++ src/components/FavPlaceEntry.tsx
@@ -4,18 +4,17 @@ interface FavPlaceProps {
   country: string;
   maps: string;
   image: {
-    src: string
-    alt: string
-  }
+    src: string;
+    alt: string;
+  };
   summary: string;
-
 }
 
 function FavPlaceEntry(props: FavPlaceProps): JSX.Element {
   return (
     <section>
       <h2>{props.title}</h2>
-      <img src={props.image.src} alt={props.image.alt}/>
+      <img src={props.image.src} alt={props.image.alt} />
       <a href={props.maps}>click me to see where this place is on a map!</a>
       <p>{props.summary}</p>
     </section>
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,18 +1,22 @@
 import FavPlaceEntry from "./FavPlaceEntry";
-import nafplio from "../images/nafplio.jpg"
+import nafplio from "../images/nafplio.jpg";
 
 function MainContent(): JSX.Element {
   return (
     <main>
-  <FavPlaceEntry
-    title ={"Nea Kios, Greece"}
-    place={"Nea Kios"}
-    country={"Greece"}
-    maps={"https://www.google.com/maps/place/%CE%9D%CE%AD%CE%B1+%CE%9A%CE%AF%CE%BF%CF%82,+%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1/@37.5885764,22.7397158,16z/data=!3m1!4b1!4m5!3m4!1s0x149ffb0a1fa28e03:0x400bd2ce2b98af0!8m2!3d37.5874845!4d22.7447696"}
-    summary ={"This is where I spent most of my summertime until I moved to England."}
-    image={{src: nafplio, alt:""}}
-  />
-  </main>
+      <FavPlaceEntry
+        title={"Nea Kios, Greece"}
+        place={"Nea Kios"}
+        country={"Greece"}
+        maps={
+          "https://www.google.com/maps/place/%CE%9D%CE%AD%CE%B1+%CE%9A%CE%AF%CE%BF%CF%82,+%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1/@37.5885764,22.7397158,16z/data=!3m1!4b1!4m5!3m4!1s0x149ffb0a1fa28e03:0x400bd2ce2b98af0!8m2!3d37.5874845!4d22.7447696"
+        }
+        summary={
+          "This is where I spent most of my summertime until I moved to England."
+        }
+        image={{ src: nafplio, alt: "" }}
+      />
+    </main>
   );
 }
 export default MainContent;
