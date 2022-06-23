af2866195d60e850e774e98406ace88a40ad32d6
Eralia <eralia.katsakiori@outlook.com>
2022-06-23 17:01:08 +0100

    CSS styling for PageHeader + MainContent + display entries from MainContent

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,22 +1,68 @@
-import FavPlaceEntry from "./FavPlaceEntry";
-import nafplio from "../images/nafplio.jpg";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function MainContent(): JSX.Element {
+  interface EntryInfo {
+    entry_id: number;
+    title: string;
+    place: string;
+    country: string;
+    maps: string;
+    summary: string;
+    image: string;
+  }
+
+  const [entries, setEntries] = useState<EntryInfo[]>();
+
+  useEffect(() => {
+    const fetchEntries = async () => {
+      const response = await axios.get("http://localhost:4000/");
+      setEntries(response.data);
+    };
+
+    fetchEntries();
+  }, []);
+
+  function displayEachEntry(entry: EntryInfo) {
+    return (
+      <div key={entry.entry_id} className="entry-block">
+        <h3> {entry.title} </h3>
+        <h4>
+          {` ${entry.place}, ${entry.country} `}
+        </h4>
+        <p>{entry.summary}</p>
+        <img src={entry.image} alt={`${entry.place}`} style={{width: 600 }} />
+        <div className="interactions-entry-block">
+        <iframe
+          src={entry.maps} 
+          title={`${entry.place} location on Google Maps`}
+          width="300"
+          height="175"
+          frameBorder="0"
+          style={{ border: 0, padding: 10 }}
+          aria-hidden="false"
+          tabIndex={0}
+          allowFullScreen={false}
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+        <button className="button-like">Like ♡</button> 
+      </div>
+      </div>      
+    );
+  }
+
+  // const myColours = [
+  //   "#413e4a", dark grey
+  //   "#73626e", greyish purple
+  //   "#b38184", sapio milo
+  //   "#f0b49e", coral
+  //   "#f7e4be"  newspaper yellow
+  // ]
+
   return (
-    <main>
-      <FavPlaceEntry
-        title={"Nea Kios, Greece"}
-        place={"Nea Kios"}
-        country={"Greece"}
-        maps={
-          "https://www.google.com/maps/place/%CE%9D%CE%AD%CE%B1+%CE%9A%CE%AF%CE%BF%CF%82,+%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1/@37.5885764,22.7397158,16z/data=!3m1!4b1!4m5!3m4!1s0x149ffb0a1fa28e03:0x400bd2ce2b98af0!8m2!3d37.5874845!4d22.7447696"
-        }
-        summary={
-          "This is where I spent most of my summertime until I moved to England."
-        }
-        image={{ src: nafplio, alt: "" }}
-      />
-    </main>
+    <>
+      <main>{entries?.map(displayEachEntry)}</main>
+    </>
   );
 }
 export default MainContent;
